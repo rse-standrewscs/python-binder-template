ed0d40b1969851435834bf7b3e260ca9f4adafda
Alexander Konovalov <alexk@mcs.st-andrews.ac.uk>
2020-06-18 12:33:42 +0100

    Define a function and use it in both .py and .ipynb files

--- code/analysis.py
+++ code/analysis.py
@@ -3,13 +3,17 @@
 import sys
 import numpy
 
+def row_means(datafile):
+    data = numpy.loadtxt(datafile, delimiter=',')
+    for row_mean in numpy.mean(data, axis=1):
+        print(row_mean)
+
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: analysis.py <datafile>") 
     else:  
-        data = numpy.loadtxt(sys.argv[1], delimiter=',')
-        for row_mean in numpy.mean(data, axis=1):
-            print(row_mean)
+        row_means(sys.argv[1])
 
 if __name__ == '__main__':
    main()
